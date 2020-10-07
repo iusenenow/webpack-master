9dc8c61bb6a25e0c74fb97d667f6bc3b6c6dadfb
Wei Zhang <iusenenow@gmail.com>
2020-10-07 23:01:08 +1100

    minor change

--- webpack.config.js
+++ webpack.config.js
@@ -10,5 +10,19 @@ module.exports = {
     port: 8080,
     contentBase: path.resolve(__dirname, 'dist'),
     hot: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
   }
 }
