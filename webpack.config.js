881544320d79664a6a70cea0aa247d52fcef256a
Wei Zhang <iusenenow@gmail.com>
2020-10-09 00:24:21 +1100

    scss loader

--- webpack.config.js
+++ webpack.config.js
@@ -14,8 +14,8 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: ["css-loader"]
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
       },
       {
         test: /\.js$/,
